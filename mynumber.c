e8ca2effcfbd45077cca7de30503ab76b2b7c597
duzzevzn <duzzevzn@gmail.com>
2026-05-01 02:43:36 +0000

    Update mynumber.c

--- mynumber.c
+++ mynumber.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
 
 int main(void) {
+    // 홀수 출력
     for(int i = 1; i <= 100; i++) {
         if(i % 2 == 1) {
             printf("%d\n", i);
         }
     }
+
+    // 짝수 출력
+    for(int i = 1; i <= 100; i++) {
+        if(i % 2 == 0) {
+            printf("%d\n", i);
+        }
+    }
+
     return 0;
 }
