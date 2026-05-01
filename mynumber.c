85eb554448b731f02b5336db9894d4926b0e011e
duzzevzn <duzzevzn@gmail.com>
2026-05-01 03:05:46 +0000

    Add prime numbers in Testing branch

--- mynumber.c
+++ mynumber.c
@@ -15,5 +15,19 @@ int main(void) {
         }
     }
 
+    // 소수 출력
+    for(int i = 2; i <= 100; i++) {
+        int isPrime = 1;
+        for(int j = 2; j < i; j++) {
+            if(i % j == 0) {
+                isPrime = 0;
+                break;
+            }
+        }
+        if(isPrime) {
+            printf("%d\n", i);
+        }
+    }
+
     return 0;
 }
